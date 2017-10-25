5f65a310bbd8fb1c5e53cd1a12cb0b1c413e619e
filipHartman <filip.hartman23@gmail.com>
2017-10-25 20:19:31 +0200

    add appending task to todo_archive

--- model.py
+++ model.py
@@ -3,5 +3,8 @@ class ToDo_archive():
     def __init__(self):
         self.todo_archive = []
 
+    def add_item(self, task):
+        self.todo_archive.append(task)
+
     def delete_item(self, index):
         self.todo_archive.pop(index)
