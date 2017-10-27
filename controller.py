94fd6aabd9914d38edf1107d886e37f1b6415d49
filipHartman <filip.hartman23@gmail.com>
2017-10-27 11:51:35 +0200

    add appending new tasks to list

--- controller.py
+++ controller.py
@@ -1,10 +1,11 @@
 from model import *
 
 
-def ask_add_item():
+def ask_add_item(tasks_archive):
     name = input('Enter the name of task to do: ')
     description = input('Enter the description of task to do: ')
     task = ToDo_item(name, description)
+    tasks_archive.todo_archive.append(task)
 
 def ask_delete_item():
     pass
