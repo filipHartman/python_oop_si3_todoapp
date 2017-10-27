1486c3a2b41e3a7939aaa9425c1cb0352959dc8d
filipHartman <filip.hartman23@gmail.com>
2017-10-27 10:41:28 +0200

    delete class and initialize functions for controller

--- controller.py
+++ controller.py
@@ -1,2 +1,21 @@
-class Controller_ToDo():
+from model import *
+
+
+def ask_add_item():
+    pass
+
+
+def ask_delete_item():
+    pass
+
+
+def ask_modify_item():
+    pass
+
+
+def ask_display_all_items():
+    pass
+
+
+def ask_display_item_details():
     pass
